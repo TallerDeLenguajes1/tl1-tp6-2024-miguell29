c238d057ecb997dfeb52ebe92023acc4d0bf1119
Miguel Juarez <mfjuaarez@gmail.com>
2024-06-21 21:27:40 -0300

    Se completo los ejercicios propuestos

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int a;
+        int b;
+        a = 10;
+        b = a;
+        Console.WriteLine("valor de a: " + a);
+        Console.WriteLine("valor de b : " + b);
+    }
+}
