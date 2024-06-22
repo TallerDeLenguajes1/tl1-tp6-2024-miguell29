d9bf71115ecdba2ec8604d286bd3b69f1cf1bdc5
Miguel Juarez <mfjuaarez@gmail.com>
2024-06-21 22:15:20 -0300

    Se completo el ejercicio n°1

--- Program.cs
+++ Program.cs
@@ -2,12 +2,35 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        int a;
-        int b;
-        a = 10;
-        b = a;
-        Console.WriteLine("valor de a: " + a);
-        Console.WriteLine("valor de b : " + b);
+        int numero;
+        string input;
+        Console.WriteLine("Ingrese un Número positivo");
+        Boolean salir = false;
+        do
+        {
+            input = Console.ReadLine();
+            if (int.TryParse(input,out numero))
+            {
+                if (numero > 0)
+                {
+                    int numeroInvertido = 0, aux;
+                    while (numero != 0)
+                    {
+                        aux = numero % 10;
+                        numeroInvertido = numeroInvertido * 10 + aux;
+                        numero /= 10;
+                    }
+
+                    Console.WriteLine($"El número invertido es: {numeroInvertido}");
+                    salir = true;
+                }else
+                {
+                    Console.WriteLine("ERROR - ingrese un Número positivo");
+                }
+            }else
+            {
+                Console.WriteLine("ERROR - Ingrese un Número");
+            }   
+        } while (!salir);
     }
 }
