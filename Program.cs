2fdf1025e58a00a71c3aa2a47e33c267b3e00bab
Miguel Juarez <mfjuaarez@gmail.com>
2024-06-22 15:18:17 -0300

    Se completo el ejercicio n°4

--- Program.cs
+++ Program.cs
@@ -2,35 +2,111 @@
 {
     private static void Main(string[] args)
     {
-        int numero;
-        string input;
-        Console.WriteLine("Ingrese un Número positivo");
-        Boolean salir = false;
-        do
-        {
-            input = Console.ReadLine();
-            if (int.TryParse(input,out numero))
-            {
-                if (numero > 0)
-                {
-                    int numeroInvertido = 0, aux;
-                    while (numero != 0)
-                    {
-                        aux = numero % 10;
-                        numeroInvertido = numeroInvertido * 10 + aux;
-                        numero /= 10;
-                    }
+        string input, input2, palabra;
 
-                    Console.WriteLine($"El número invertido es: {numeroInvertido}");
-                    salir = true;
-                }else
-                {
-                    Console.WriteLine("ERROR - ingrese un Número positivo");
+        Console.WriteLine("Ingrese una cadena de texto");
+        input = Console.ReadLine();
+        Console.WriteLine($"La longitud de la cadena ingresada es: {input.Length}");
+
+        Console.WriteLine("Ingrese otra cadena de texto");
+        input2 = Console.ReadLine();
+        Console.WriteLine(input +" "+ input2);
+
+        Console.WriteLine($"Los primeros 4 caracteres de la primer cadena son: '{input.Substring(0,4)}'");
+        Console.WriteLine($"Los primeros 4 caracteres de la segunda cadena son: '{input2.Substring(0,4)}'");
+        /*
+        Utilizando la calculadora creada anteriormente realizar las operaciones de dosnúmeros y mostrar por pantalla y mostrar en texto el resultado.
+        Por ejemplo parala suma sería:“la suma de “ num1 “ y de” num2 “ es igual a: ” resultado.
+        !Este ejercicio ya lo realice dentro de la rama calculadora v1
+        */
+
+        Console.WriteLine("Recorriendo la cadena mediante foreach: ");
+        foreach (var letra in input)
+        {
+            Console.Write(letra + " ");
+        }
+
+        Console.WriteLine("ingrese la palabra que desea buscar en la cadena:");
+        palabra = Console.ReadLine();
+        int indice = input.IndexOf(palabra,StringComparison.CurrentCultureIgnoreCase);
+        if(indice != -1){
+            Console.WriteLine($"Se encontro la palabra {palabra} en la posicion {indice+1} de la cadena {input}");
+        }
+        else{
+            Console.WriteLine("no se encuentro la palabra");
+        }
+        
+        Console.WriteLine($"La primera cadena en minúsculas queda: {input.ToLower()}");
+        Console.WriteLine($"La segunda cadena en mayúsculas queda: {input2.ToUpper()}");
+
+        Console.WriteLine("Ingrese una cadena separada por espacios");
+        string input3 = Console.ReadLine();
+        string[] palabras = input3.Split(" ");
+        foreach(string letra in palabras){
+            Console.WriteLine(letra);
+        }
+        Boolean error;
+        do
+        {          
+            Console.WriteLine("----------Que desea calcular? (suma - resta -multiplicacion - divicion)----------");
+            Console.WriteLine("ingrese una ecuacion:");
+            string cadenaEcuacion = Console.ReadLine();
+            error = true;
+            int num1 = 0, num2 = 0;
+            foreach(char caracter in cadenaEcuacion){
+                if(caracter == '+'){
+                    string[] cadenaEcuacionSeparada = cadenaEcuacion.Trim().Split('+');
+                    if (int.TryParse(cadenaEcuacionSeparada[0],out num1) && int.TryParse(cadenaEcuacionSeparada[1],out num2))
+                    {
+                        Console.WriteLine($"El resultado es: {num1 + num2}");
+                        error = false;
+                        break;
+                    }
+                        Console.WriteLine("ERROR - Ingrese numeros validos");
+                        break;
                 }
-            }else
-            {
-                Console.WriteLine("ERROR - Ingrese un Número");
-            }   
-        } while (!salir);
+                else if(caracter == '-'){
+                    string[] cadenaEcuacionSeparada = cadenaEcuacion.Trim().Split('-');
+                    if (int.TryParse(cadenaEcuacionSeparada[0],out num1) && int.TryParse(cadenaEcuacionSeparada[1],out num2))
+                    {
+                        Console.WriteLine($"El resultado es: {num1 - num2}");
+                        error = false;
+                        break;
+                    }
+                    Console.WriteLine("ERROR - Ingrese numeros validos");
+                    break;
+                }
+                else if(caracter == '*'){
+                    string[] cadenaEcuacionSeparada = cadenaEcuacion.Trim().Split('*');
+                    if (int.TryParse(cadenaEcuacionSeparada[0],out num1) && int.TryParse(cadenaEcuacionSeparada[1],out num2))
+                    {
+                        Console.WriteLine($"El resultado es: {num1 * num2}");
+                        error = false;
+                        break;
+                    }
+                    Console.WriteLine("ERROR - Ingrese numeros validos");
+                    break;
+                }
+                else if(caracter == '/'){
+                    string[] cadenaEcuacionSeparada = cadenaEcuacion.Trim().Split('/');
+                    if (int.TryParse(cadenaEcuacionSeparada[0],out num1) && int.TryParse(cadenaEcuacionSeparada[1],out num2))
+                    {
+                        if (num2 == 0)
+                        {
+                            Console.WriteLine("ERROR - No se puede dividir en 0");
+                            break;
+                        }
+                        Console.WriteLine($"El resultado es: {num1 / num2}");
+                        error = false;
+                        break;
+                    }
+                    Console.WriteLine("ERROR - Ingrese numeros validos");
+                    break;
+                }
+                
+            }
+        } while (error);
+            
     }
+        
 }
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Taller de Lenguajes 1 - TP6 - Miguel Juárez
+##### ¿String es una tipo por valor o un tipo por referencia?
+
+- Un string es un tipo por referencia.
+
+##### ¿Qué secuencias de escape tiene el tipo string?
+
+|Secuencia de escape |	Nombre de carácter |	Codificación Unicode|
+|--------------------|---------------------|------------------------|
+|\' |	Comilla simple |	0x0027|
+|\" 	|Comilla doble |	0x0022|
+|\ 	| Barra diagonal inversa| 	0x005C
+|\0 |	Null 	|0x0000|
+|\a 	|Alerta 	|0x0007|
+|\b 	|Retroceso |	0x0008|
+|\f 	|Avance de página| 	0x000C|
+|\n 	|Nueva línea |	0x000A|
+|\r 	|Retorno de carro |	0x000D|
+|\t 	|Tabulación horizontal |	0x0009|
+|\v 	|Tabulación vertical |	0x000B|
+|\u 	|Secuencia de escape Unicode (UTF-16) |	\uHHHH (intervalo: 0000 - FFFF; ejemplo: \u00E7 = "ç")|
+|\U 	|Secuencia de escape Unicode (UTF-32) |	\U00HHHHHH (intervalo: 000000 - 10FFFF; ejemplo: \U0001F47D = "👽")|
+|\x 	|Secuencia de escape Unicode similar a "\u" excepto con longitud variable |	\xH[H][H][H] (intervalo: 0 - FFFF ejemplo: \x00E7 o \x0E7 o \xE7 = "ç")|
+
+##### ¿Qué sucede cuando utiliza el carácter @ y $ antes de una cadena de texto?
+
+- Los caracteres @ y $ se utilizan como prefijos antes de las cadenas de texto para proporcionar funcionalidades adicionales. A continuación, se explica el propósito y el uso de cada uno de estos caracteres:
+
+###### @ - Cadena literal (Verbatim String)
+
+- El prefijo @ se utiliza para crear cadenas literales, donde las secuencias de escape no se procesan y los caracteres especiales se interpretan como texto literal. Además, permite que las cadenas de texto se extiendan en múltiples líneas sin necesidad de concatenación explícita.
+
+###### $ - Interpolación de cadenas (String Interpolation)
+
+- El prefijo $ se utiliza para crear cadenas interpoladas, donde se pueden incluir expresiones dentro de llaves {} que serán evaluadas y sus resultados insertados en la cadena.
+
+######W Combinación de @ y $ (Literal y Interpolada)
+
+- Es posible combinar ambos prefijos @$ o $@ para crear una cadena que sea tanto literal como interpolada. Esto permite que las expresiones dentro de {} sean evaluadas mientras que las secuencias de escape se interpretan literalmente.
