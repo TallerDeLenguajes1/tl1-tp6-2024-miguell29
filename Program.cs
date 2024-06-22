18c120dc2bfed7b5a6e406de9795bfe4b642a5bc
Miguel Juarez <mfjuaarez@gmail.com>
2024-06-21 23:24:18 -0300

    Se completo el ejercicio 2

--- Program.cs
+++ Program.cs
@@ -2,35 +2,92 @@
 {
     private static void Main(string[] args)
     {
-        int numero;
+        int opcion, num1, num2;
         string input;
-        Console.WriteLine("Ingrese un Número positivo");
         Boolean salir = false;
+        Console.Clear();
         do
         {
-            input = Console.ReadLine();
-            if (int.TryParse(input,out numero))
-            {
-                if (numero > 0)
-                {
-                    int numeroInvertido = 0, aux;
-                    while (numero != 0)
-                    {
-                        aux = numero % 10;
-                        numeroInvertido = numeroInvertido * 10 + aux;
-                        numero /= 10;
-                    }
+            Console.WriteLine("--------------->");
+            Console.WriteLine("---------------> Calculadora version 1.0");
+            Console.WriteLine("--------------->");
+            Console.WriteLine("1 - Sumar");
+            Console.WriteLine("2 - Restar");
+            Console.WriteLine("3 - Multiplicar");
+            Console.WriteLine("4 - Dividir");
+            Console.WriteLine("0 - Salir");
+            Console.WriteLine("Ingrese una opcion:");
 
-                    Console.WriteLine($"El número invertido es: {numeroInvertido}");
+            input = Console.ReadLine();
+            if (int.TryParse(input,out opcion))
+            {   
+                if (opcion == 0)
+                {
                     salir = true;
+                    Console.WriteLine("Saliendo...");
+                }
+                if (opcion > 0 && opcion < 5)
+                {
+                    Console.WriteLine("Ingrese el primer número");
+                    input = Console.ReadLine();
+                    if (!int.TryParse(input, out num1))
+                    {
+                        Console.Clear();
+                        Console.WriteLine("ERROR - Intentelo de nuevo");
+                        continue;
+                    }
+                    Console.WriteLine("Ingrese el segundo número");
+                    input = Console.ReadLine();
+                    if (!int.TryParse(input, out num2))
+                    {
+                        Console.Clear();
+                        Console.WriteLine("ERROR - Intentelo de nuevo");
+                        continue;
+                    }
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+                        break;
+                        case 2:
+                            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                        break;
+                        case 3:
+                            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                        break;
+                        case 4:
+                            Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+                        break;
+                    }
+                    do
+                    {
+                        Console.WriteLine("Desea realizar otra operacion?  (y/n)");
+                        input = Console.ReadLine();
+                        if (input == "n" || input == "N")
+                        {
+                            Console.WriteLine("saliendo...");
+                            salir = true;
+                            break;
+                        }else
+                        {
+                            if (!(input == "y" || input == "Y") )
+                            {
+                                Console.Clear();
+                                Console.WriteLine("ERROR - Opción no válida");
+                            }
+                        }
+                    } while (!(input == "y" || input == "Y"));
                 }else
                 {
-                    Console.WriteLine("ERROR - ingrese un Número positivo");
+                    Console.Clear();
+                    Console.WriteLine("ERROR - Opción no válida");
                 }
             }else
             {
-                Console.WriteLine("ERROR - Ingrese un Número");
-            }   
+                Console.Clear();
+                Console.WriteLine("ERROR - Opción no válida");
+            }
+            
         } while (!salir);
-    }
+    }        
 }
